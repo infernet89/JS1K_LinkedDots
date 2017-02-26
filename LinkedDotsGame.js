3749eb9a319c868c8df8398f3a6d77a2760b6e7d
Infernet89 <infernet2002@libero.it>
2017-02-26 19:19:52 +0100

    1067

--- LinkedDotsGame.js
+++ LinkedDotsGame.js
@@ -1,50 +1,50 @@
 //< >
-setInterval(run, 33);
-var points=[];
-var links=[];
-var level=3;
+setInterval(rn, 33);
+var p=[];
+var l=[];
+var lvl=3;
 var mx;
 var my;
-var clicked=false;
-var draggingPoint=-1;
-var desperation=0;
+var ck=0;
+var dp=-1;
+var ds=0;
 a.addEventListener("mousemove", function (e) { mx=e.clientX; my=e.clientY;});
 a.addEventListener("mousedown", function (e) {
-  clicked=true;
-  for(i in points)
-    if( distance(points[i].x, points[i].y,mx,my) < 400)
-      draggingPoint=i;
+  ck=1;
+  for(i in p)
+    if( di(p[i].x, p[i].y,mx,my) < 400)
+      dp=i;
       
 });
-a.addEventListener("mouseup", function (e) { clicked=false; draggingPoint=-1;});
-function generateLevel()
+a.addEventListener("mouseup", function (e) { ck=0; dp=-1;});
+function gl()
 {
-  level++;
-  points=[];
-  links=[];
-  //points
-  for(i=0;i<level;i++)
-    points.push({ x: Math.random()*a.width , y:y=Math.random()*a.height});
-  //links
-  for(i=0;i<level;i++)
-    for(k=i+1;k<level;k++)
-      links.push({from: i, to: k});
+  lvl++;
+  p=[];
+  l=[];
+  //p
+  for(i=0;i<lvl;i++)
+    p.push({ x: Math.random()*a.width , y:y=Math.random()*a.height});
+  //l
+  for(i=0;i<lvl;i++)
+    for(k=i+1;k<lvl;k++)
+      l.push({from: i, to: k});
   //make it solvable, kinda.
-  for(k=4;k<level;k++)
-    for(i=k;i<level;i++)
-      links.splice(~~(Math.random()*links.length),1)
+  for(k=4;k<lvl;k++)
+    for(i=k;i<lvl;i++)
+      l.splice(~~(Math.random()*l.length),1)
 }
-function indipendenti(a,b,a1,b1)
+function id(a,b,a1,b1)
 {
   //console.log("Controllo "+a+"-"+b+"  con  "+a1+"-"+b1);
-  var x = points[a].x;
-  var y = points[a].y;
-  var x1 = points[b].x;
-  var y1 = points[b].y;
-  var x2 = points[a1].x;
-  var y2 = points[a1].y;
-  var x3 = points[b1].x;
-  var y3 = points[b1].y;
+  var x = p[a].x;
+  var y = p[a].y;
+  var x1 = p[b].x;
+  var y1 = p[b].y;
+  var x2 = p[a1].x;
+  var y2 = p[a1].y;
+  var x3 = p[b1].x;
+  var y3 = p[b1].y;
   //TRUST PAST ME.
   if (
     ((y3-y2) * (x1-x2) / (x3-x2+0.1) + y2 - y1) * 
@@ -52,94 +52,94 @@ function indipendenti(a,b,a1,b1)
     && 
     ((y1-y)  * (x2-x) /  (x1-x +0.1) + y  - y2) * 
     ((y1-y)  * (x3-x) /  (x1-x +0.1) + y  - y3) < 0)
-    return false;
-  return true;
+    return 0;
+  return 1;
 }
-function distance(ax,ay,bx,by)
+function di(ax,ay,bx,by)
 {
   return (ax-bx)*(ax-bx)+(ay-by)*(ay-by);
 }
-function drawPoint(x,y,color)
+function dt(x,y,color)
 {
   c.fillStyle=color;
   c.beginPath();
   c.arc(x,y,15,0,6);
   c.fill();
 }
-function drawAllConnections()
+function da()
 {
   c.lineWidth=2;
-  for(k in links)
+  for(k in l)
   {
-    c.strokeStyle=links[k].c;
+    c.strokeStyle=l[k].c;
     c.beginPath();
-    c.moveTo(points[links[k].from].x,points[links[k].from].y);
-    c.lineTo(points[links[k].to].x,points[links[k].to].y);
+    c.moveTo(p[l[k].from].x,p[l[k].from].y);
+    c.lineTo(p[l[k].to].x,p[l[k].to].y);
     c.stroke();
   }
   return;
 }
 //controlla e colora
-function check()
+function ch()
 {
-  allSafe=true;
+  allSafe=1;
   //collisioni dei punti
-  for(i in points)
+  for(i in p)
   {
-    points[i].color="Blue";
-    for(k in points)
+    p[i].color="Blue";
+    for(k in p)
     {
       if(i==k)
         continue;
-      if(distance(points[i].x,points[i].y,points[k].x,points[k].y)<400)
+      if(di(p[i].x,p[i].y,p[k].x,p[k].y)<400)
       {
-        points[i].color="Red";
-        allSafe=false;
+        p[i].color="Red";
+        allSafe=0;
         break;
       }
     }
   }
   //reset colori
-  for(k in links)
-    links[k].c="Blue";
+  for(k in l)
+    l[k].c="Blue";
   //collisioni dei link
-  for(i in links)
-    for(k in links)
+  for(i in l)
+    for(k in l)
     {
-      if(i==k || links[i].from==links[k].from || links[i].to==links[k].to || links[i].from==links[k].to || links[i].to==links[k].from)
+      if(i==k || l[i].from==l[k].from || l[i].to==l[k].to || l[i].from==l[k].to || l[i].to==l[k].from)
         continue;
-      if(!indipendenti(links[i].from, links[i].to, links[k].from, links[k].to))
+      if(!id(l[i].from, l[i].to, l[k].from, l[k].to))
           {
-            links[i].c="Red";
-            //links[k].c="Red";
-            allSafe=false;
+            l[i].c="Red";
+            //l[k].c="Red";
+            allSafe=0;
           }
     }
   return allSafe;
 }
-function run()
+function rn()
 {
 	//draw the background
 	c.fillStyle="Silver";
 	c.fillRect(0,0,a.width,a.height);
 
-  if(clicked && draggingPoint > -1)
+  if(ck && dp > -1)
   {
-    points[draggingPoint].x=mx;
-    points[draggingPoint].y=my;
+    p[dp].x=mx;
+    p[dp].y=my;
   }
-  if(check())
-    generateLevel();
-  //Draw the points
-  for(i=0;i<level;i++)
-    drawPoint(points[i].x,points[i].y,points[i].color);
+  if(ch())
+    gl();
+  //Draw the p
+  for(i=0;i<lvl;i++)
+    dt(p[i].x,p[i].y,p[i].color);
     
 
-  drawAllConnections();
+  da();
   //if you are desperated, we make the game easier
-  if(desperation++>1000)
+  if(ds++>1000)
   {
-    desperation=0;
-    links.splice(~~(Math.random()*links.length),1)
+    ds=0;
+    l.splice(~~(Math.random()*l.length),1)
   }
 }
